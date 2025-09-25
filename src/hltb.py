e7715c1c9b3abd3f331e5027900fdc036596f418
Meeman <meeman_3@outlook.com>
2025-09-25 08:12:06 +0100

    a

--- src/hltb.py
+++ src/hltb.py
@@ -3,7 +3,7 @@ from howlongtobeatpy import HowLongToBeat
 
 def game_add_hltb(game_dict):
     game_hltb = None
-    game_name = game_dict["name"].replace("Edition", "").replace("™", "")
+    game_name = game_dict["name"].replace("™", "").lower().replace("edition", "")
     game_hltb_list = HowLongToBeat().search(game_name)
     if game_hltb_list is not None and len(game_hltb_list) > 0:
         game_hltb = max(game_hltb_list, key=lambda element: element.similarity)
@@ -14,7 +14,6 @@ def game_add_hltb(game_dict):
         game_dict["hltb"] = game_hltb.main_extra
     
     return game_dict
-
 
 
 
